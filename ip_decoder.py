23d852f1dbcf1a60d2ae396d52625becfdffd5d6
Sharpshooter4514 <lcrosa1998@gmail.com>
2023-10-06 11:08:05 -0500

    Made Changes

--- ip_decoder.py
+++ ip_decoder.py
@@ -28,7 +28,7 @@ class IP:
         self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
 
         try:
-            self.protocol_map[self.protocol_num]
+            self.protocol = self.protocol_map[self.protocol_num]
         except Exception as e:
             print('%s No protocol for %s' % (e, self.protocol_num))
             self.protocol = str(self.protocol_num)
