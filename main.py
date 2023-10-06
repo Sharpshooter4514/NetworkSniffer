aab170de70dfe7f6e8e4b8b50cc3e54a1d560fc6
Sharpshooter4514 <lcrosa1998@gmail.com>
2023-10-06 09:25:52 -0500

    initial program

---
+++ main.py
@@ -0,0 +1,30 @@
+import socket
+import os
+
+# Host to listen on
+host = '192.168.1.203'
+
+def main():
+    # create raw socket, bin to public interface
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((host, 0))
+    # Include the IP header in the capture
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    # Read one packet
+    print(sniffer.recvfrom(65565))
+
+    # If we're on Windows, turn off promiscuious mode
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)\
+
+if __name__ == '__main__':
+    main()
